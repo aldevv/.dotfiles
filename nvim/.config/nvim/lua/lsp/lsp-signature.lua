0b0ef123a0b110a37d529e6467ae8edd30ce27d4
Alejandro Bernal <jbernal@unal.edu.co>
2022-07-17 22:39:16 -0500

    updated copyq, config and newboat

--- nvim/.config/nvim/lua/lsp/lsp-signature.lua
+++ nvim/.config/nvim/lua/lsp/lsp-signature.lua
@@ -15,6 +15,7 @@
   max_width = 80, -- max_width of signature floating_window
   wrap = false, -- allow doc/signature text wrap inside floating_window, useful if your lsp return doc/sig is too long
 
+  -- this enables the panda
   floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
 
   floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
@@ -47,7 +48,8 @@
 
   transparency = nil, -- disabled by default, allow floating win transparent value 1~100
   shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+  -- shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
+  shadow_guibg = 'Green', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
   timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
   toggle_key = nil, -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
 
