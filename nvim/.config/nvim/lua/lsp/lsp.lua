3d35764ad9a12f861065fe41c4a1855e3a69d48d
Alejandro Bernal <jbernal@unal.edu.co>
2022-09-08 12:46:17 -0500

    added sqls better config, fixed bug where all servers would start

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -36,42 +36,30 @@ local opts = {
     on_attach = lsp_defaults.on_attach,
 }
 
-local no_defaults = { "rust_analyzer" }
-require("mason-lspconfig").setup_handlers({
-    function(server)
-        for _, exception in ipairs(no_defaults) do
-            if server == exception then
-                return
-            end
-        end
+local exceptions = { rust_analyzer = "rust-tools" }
+for _, server in ipairs(servers) do
+    local is_exception = false
 
-        if lang_opts.enhanceable(server) then
-            lang_opts.enhance(server, opts)
+    local opts = {
+        capabilities = lsp_defaults.capabilities,
+        handlers = lsp_defaults.handlers,
+        on_attach = lsp_defaults.on_attach,
+    }
+    if lang_opts.enhanceable(server) then
+        lang_opts.enhance(server, opts)
+    end
+
+    for exception, map in pairs(exceptions) do
+        if server == exception then
+            require(map).setup(opts)
+            is_exception = true
         end
+    end
+
+    if not is_exception then
         require("lspconfig")[server].setup(opts)
-    end,
-
-    ["rust_analyzer"] = function()
-        require("rust-tools").setup({
-            server = {
-                capabilities = opts.capabilities,
-                handlers = opts.handlers,
-                on_attach = function(cl, bufnr)
-                    opts.on_attach(cl, bufnr)
-                    -- hover
-                    vim.keymap.set("n", "+", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
-                    -- Code action groups
-                    vim.keymap.set(
-                        "n",
-                        "<Leader>la",
-                        require("rust-tools").code_action_group.code_action_group,
-                        { buffer = bufnr }
-                    )
-                end,
-            },
-        })
-    end,
-})
+    end
+end
 
 -- diagnostics
 vim.diagnostic.config({
