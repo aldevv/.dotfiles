36b44d97d5c4fcff8e1e129b0a4633c8e991d01a
Alejandro Bernal <jbernal@unal.edu.co>
2022-11-14 22:18:16 -0500

    added mason support for lsp

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -27,6 +27,17 @@ require("mason-lspconfig").setup({
     ensure_installed = servers,
 })
 
+require("mason-lspconfig").setup_handlers({
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup({})
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+})
+
 local lsp_defaults = require("lsp.lsp_defaults")
 local lang_opts = require("lsp.lang_opts")
 
@@ -57,7 +68,7 @@ for _, server in ipairs(servers) do
     end
 
     if not is_exception then
-    require("lspconfig")[server].setup(opts)
+        require("lspconfig")[server].setup(opts)
     end
 end
 
