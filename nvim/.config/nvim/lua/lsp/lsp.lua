b22d8347e98c840e32fd854be0dce0284c7364bc
Alejandro Bernal <jbernal@unal.edu.co>
2022-11-14 22:34:28 -0500

    added better mason config that actually uses up the manually installed lsp's

--- nvim/.config/nvim/lua/lsp/lsp.lua
+++ nvim/.config/nvim/lua/lsp/lsp.lua
@@ -27,50 +27,35 @@ require("mason-lspconfig").setup({
     ensure_installed = servers,
 })
 
-require("mason-lspconfig").setup_handlers({
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup({})
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-})
-
-local lsp_defaults = require("lsp.lsp_defaults")
-local lang_opts = require("lsp.lang_opts")
-
-local opts = {
-    capabilities = lsp_defaults.capabilities,
-    handlers = lsp_defaults.handlers,
-    on_attach = lsp_defaults.on_attach,
-}
-
-local exceptions = { rust_analyzer = "rust-tools" }
-for _, server in ipairs(servers) do
-    local is_exception = false
-
-    local opts = {
+local get_opts = function()
+    local lsp_defaults = require("lsp.lsp_defaults")
+    return {
         capabilities = lsp_defaults.capabilities,
         handlers = lsp_defaults.handlers,
         on_attach = lsp_defaults.on_attach,
     }
+end
+
+local enhance_server = function(server, opts)
+    local lang_opts = require("lsp.lang_opts")
     if lang_opts.enhanceable(server) then
         lang_opts.enhance(server, opts)
     end
-
-    for exception, map in pairs(exceptions) do
-        if server == exception then
-            require(map).setup(opts)
-            is_exception = true
-        end
-    end
-
-    if not is_exception then
-        require("lspconfig")[server].setup(opts)
-    end
 end
+
+require("mason-lspconfig").setup_handlers({
+    function(server_name) -- default handler (optional)
+        local opts = get_opts()
+        enhance_server(server_name, opts)
+        require("lspconfig")[server_name].setup(opts)
+    end,
+
+    ["rust_analyzer"] = function()
+        local opts = get_opts()
+        enhance_server("rust_analyzer", opts)
+        require("rust-tools").setup(opts)
+    end,
+})
 
 -- diagnostics
 vim.diagnostic.config({
