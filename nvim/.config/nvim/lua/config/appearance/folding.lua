5a423f3ffb6dec1e0e9fe78bea0b04b60db3c523
Alejandro Bernal <jbernal@unal.edu.co>
2022-12-09 18:34:21 -0500

    added better nt, so it works with searching .archives, and refactored to make it easier to work with

--- nvim/.config/nvim/lua/config/appearance/folding.lua
+++ nvim/.config/nvim/lua/config/appearance/folding.lua
@@ -8,8 +8,8 @@ vim.o.foldlevel = 20
 vim.cmd([[
 augroup remember_folds
   autocmd!
-  autocmd BufWinLeave * mkview
-  autocmd BufWinEnter * silent! loadview
+  autocmd BufWinLeave *.* mkview
+  autocmd BufWinEnter *.* silent! loadview
 augroup END
 ]])
 
