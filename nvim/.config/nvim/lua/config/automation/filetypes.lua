e25c332c3b68284f96ca8793a86e00aa99b5f79f
Alejandro Bernal <jbernalxyz@gmail.com>
2023-06-09 17:19:45 -0500

    fixed clone and also cmp-cmdline

--- nvim/.config/nvim/lua/config/automation/filetypes.lua
+++ nvim/.config/nvim/lua/config/automation/filetypes.lua
@@ -1,50 +1,57 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "launch.json",
-  callback = function()
-    -- this combines snippets for launch specific and json filetypes
-    vim.opt.filetype = "launchjson"
-    vim.opt.syntax = "json"
-  end,
+	pattern = "launch.json",
+	callback = function()
+		-- this combines snippets for launch specific and json filetypes
+		vim.opt.filetype = "launchjson"
+		vim.opt.syntax = "json"
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = "docker-compose.json",
-  callback = function()
-    -- this combines snippets for launch specific and json filetypes
-    vim.opt.filetype = "docker-compose"
-    vim.opt.syntax = "yaml"
-  end,
+	pattern = "docker-compose.json",
+	callback = function()
+		-- this combines snippets for launch specific and json filetypes
+		vim.opt.filetype = "docker-compose"
+		vim.opt.syntax = "yaml"
+	end,
 })
 
 -- autoload _skel
 vim.api.nvim_create_autocmd("BufNewFile", {
-  callback = function()
-    -- NOTE: by neovim 9.2 this should work without this var and the pattern
-    local ft = vim.bo.ft
-    vim.api.nvim_create_autocmd({ "BufEnter" }, {
-      pattern = "*." .. ft,
-      callback = function()
-        -- if buffer is empty
-        if vim.fn.line("$") ~= 1 or vim.fn.getline(1) ~= "" then
-          return
-        end
-        local snips = require("luasnip").get_snippets()[vim.bo.ft]
-        for _, snip in ipairs(snips) do
-          if snip["name"] == "_skel" then
-            require("luasnip").snip_expand(snip)
-            return true
-          end
-        end
-      end,
-    })
-  end,
+	callback = function()
+		-- NOTE: by neovim 9.2 this should work without this var and the pattern
+		local ft = vim.bo.ft
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			pattern = "*." .. ft,
+			callback = function()
+				-- if buffer is empty
+				if vim.fn.line("$") ~= 1 or vim.fn.getline(1) ~= "" then
+					return
+				end
+				local snips = require("luasnip").get_snippets()[vim.bo.ft]
+				for _, snip in ipairs(snips) do
+					if snip["name"] == "_skel" then
+						require("luasnip").snip_expand(snip)
+						return true
+					end
+				end
+			end,
+		})
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  pattern = "*Neotest Summary*",
-  callback = function()
-    vim.keymap.set("n", "n", "j", { silent = true, buffer = 0 })
-    vim.keymap.set("n", "e", "k", { silent = true, buffer = 0 })
-    -- vim.keymap.set("n", "<space>", "lua neotest.Config.summary.mappings
-  end,
+	pattern = "*Neotest Summary*",
+	callback = function()
+		vim.keymap.set("n", "n", "j", { silent = true, buffer = 0 })
+		vim.keymap.set("n", "e", "k", { silent = true, buffer = 0 })
+		-- vim.keymap.set("n", "<space>", "lua neotest.Config.summary.mappings
+	end,
+})
+
+-- for bug where cmp doesn't close on win enter
+vim.api.nvim_create_autocmd("CmdWinEnter", {
+	callback = function()
+		require("cmp").close()
+	end,
 })
--- nvim/.config/nvim/lua/lsp/cmp.lua
+++ nvim/.config/nvim/lua/lsp/cmp.lua
@@ -3,32 +3,32 @@
 -- ========
 local lspkind = require("lspkind")
 lspkind.init({
-    -- preset: 'default' or  'codicons'
-    -- preset = "default",
-    -- with_text = true,
-    mode = "symbol_text",
-    symbol_map = {
-        Text = "",
-        Method = "ƒ",
-        Function = "ﬦ",
-        Constructor = "",
-        Variable = "",
-        Class = "",
-        Interface = "ﰮ",
-        Module = "",
-        Property = "",
-        Unit = "",
-        Value = "",
-        Enum = "了",
-        Keyword = "",
-        Snippet = "﬌",
-        Color = "",
-        File = "",
-        Folder = "",
-        EnumMember = "",
-        Constant = "",
-        Struct = "",
-    },
+  -- preset: 'default' or  'codicons'
+  -- preset = "default",
+  -- with_text = true,
+  mode = "symbol_text",
+  symbol_map = {
+    Text = "",
+    Method = "ƒ",
+    Function = "ﬦ",
+    Constructor = "",
+    Variable = "",
+    Class = "",
+    Interface = "ﰮ",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "了",
+    Keyword = "",
+    Snippet = "﬌",
+    Color = "",
+    File = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+  },
 })
 
 -- ==========
@@ -40,138 +40,159 @@ local luasnip = require("luasnip")
 vim.api.nvim_set_option("completeopt", "menu,menuone,noselect")
 
 cmp.setup({
-    preselect = cmp.PreselectMode.None, -- so it doesn't select lsp automatically, and lets me choose manually
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    -- NOTE: ultisnips does not work well with lsp_signature and cmp_lsp capabilities
-    -- snippet = {
-    --     expand = function(args)
-    --         -- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-    --         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    --     end,
-    -- },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    formatting = {
-        format = function(entry, vim_item)
-            vim_item.kind = string.format("%s %s", lspkind.presets.default[vim_item.kind], vim_item.kind)
-            vim_item.menu = ({
-                -- nvim_lsp = "ﲳ",
-                -- nvim_lsp = "📚",
-                luasnip = "🔥",
-                nvim_lua = "[nvim]",
-                nvim_lsp = "[LS]",
-                -- nvim_lua = "[API]",
-                -- treesitter = "",
-                treesitter = "🌲",
-                -- path = "ﱮ",
-                path = "📁",
-                -- buffer = "﬘",
-                buffer = "[BUF]",
-                zsh = "[SH]",
-                -- vsnip = "",
-                -- ultisnips = "🔥",
-                -- spell = "暈",
-                spell = "暈",
-                cmdline = "[CMD]",
-                ["vim-dadbod-completion"] = "[DB]",
-            })[entry.source.name]
-
-            return vim_item
-        end,
-    },
-    experimental = {
-        ghost_text = true,
-        -- native_menu = true
-    },
-    mapping = {
-        ["<a-e>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        ["<a-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-        ["<a-u>"] = function(fallback)
-            if cmp.visible() then
-                for _ = 1, 2 do
-                    cmp.select_prev_item()
-                end
-            else
-                fallback()
-            end
-        end,
-        ["<a-d>"] = function(fallback)
-            if cmp.visible() then
-                for _ = 1, 2 do
-                    cmp.select_next_item()
-                end
-            else
-                fallback()
-            end
-        end,
-        ["<s-up>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<s-down>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<C-Space>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.close()
-            else
-                cmp.complete()
-            end
-        end, { "i", "c" }),
-        ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert, -->https://github.com/hrsh7th/nvim-cmp/issues/664
-            -- behavior = cmp.ConfirmBehavior.Replace, -->https://github.com/hrsh7th/nvim-cmp/issues/664
-            --  check for examples https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
-            select = true, -- auto select on enter (even if not selected with <a-n>)
-        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
-    -- nvim-cmp by defaults disables autocomplete for prompt buffers
-    enabled = function()
-        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+  preselect = cmp.PreselectMode.None, -- so it doesn't select lsp automatically, and lets me choose manually
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
     end,
-    sources = {
-        { name = "luasnip", priority = 1 },
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
-        -- { name = "ultisnips" }, -- For ultisnips users.
-        -- these below also need a plugin like cmp-nvim-ultisnips
-        { name = "path", max_item_count = 10 },
-        -- { name = 'luasnip' }, TODO change to this!
-        { name = "buffer", keyword_length = 5 },
-        { name = "dap" },
-        { name = "orgmode" },
-        { name = "vim-dadbod-completion" },
-        { name = "git" },
-        -- { name = 'snippy' }, -- For snippy users.
-        -- { name = 'treesitter' },
-    },
+  },
+  -- NOTE: ultisnips does not work well with lsp_signature and cmp_lsp capabilities
+  -- snippet = {
+  --     expand = function(args)
+  --         -- require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+  --         vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+  --     end,
+  -- },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = string.format("%s %s", lspkind.presets.default[vim_item.kind], vim_item.kind)
+      vim_item.menu = ({
+            -- nvim_lsp = "ﲳ",
+            -- nvim_lsp = "📚",
+            luasnip = "🔥",
+            nvim_lua = "[nvim]",
+            nvim_lsp = "[LS]",
+            -- nvim_lua = "[API]",
+            -- treesitter = "",
+            treesitter = "🌲",
+            -- path = "ﱮ",
+            path = "📁",
+            -- buffer = "﬘",
+            buffer = "[BUF]",
+            zsh = "[SH]",
+            -- vsnip = "",
+            -- ultisnips = "🔥",
+            -- spell = "暈",
+            spell = "暈",
+            cmdline = "[CMD]",
+            ["vim-dadbod-completion"] = "[DB]",
+          })[entry.source.name]
+
+      return vim_item
+    end,
+  },
+  experimental = {
+    ghost_text = true,
+    -- native_menu = true
+  },
+  mapping = {
+    ["<a-e>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+    ["<a-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+    ["<a-u>"] = function(fallback)
+      if cmp.visible() then
+        for _ = 1, 2 do
+          cmp.select_prev_item()
+        end
+      else
+        fallback()
+      end
+    end,
+    ["<a-d>"] = function(fallback)
+      if cmp.visible() then
+        for _ = 1, 2 do
+          cmp.select_next_item()
+        end
+      else
+        fallback()
+      end
+    end,
+    ["<s-up>"] = cmp.mapping(cmp.mapping.scroll_docs( -4), { "i", "c" }),
+    ["<s-down>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.close()
+      else
+        cmp.complete()
+      end
+    end, { "i", "c" }),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert, -->https://github.com/hrsh7th/nvim-cmp/issues/664
+      -- behavior = cmp.ConfirmBehavior.Replace, -->https://github.com/hrsh7th/nvim-cmp/issues/664
+      --  check for examples https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings
+      select = true, -- auto select on enter (even if not selected with <a-n>)
+    }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  -- nvim-cmp by defaults disables autocomplete for prompt buffers
+  enabled = function()
+    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+  end,
+  sources = {
+    { name = "luasnip",              priority = 1 },
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    -- { name = "ultisnips" }, -- For ultisnips users.
+    -- these below also need a plugin like cmp-nvim-ultisnips
+    { name = "path",                 max_item_count = 10 },
+    -- { name = 'luasnip' }, TODO change to this!
+    { name = "buffer",               keyword_length = 5 },
+    { name = "dap" },
+    { name = "orgmode" },
+    { name = "vim-dadbod-completion" },
+    { name = "git" },
+    -- { name = 'snippy' }, -- For snippy users.
+    -- { name = 'treesitter' },
+  },
 })
 
 -- command line
+-- NOTE: has bug with %, where it doesn't complete it correctly
+-- cmp.setup.cmdline(":", {
+-- 	mapping = cmp.mapping.preset.cmdline(),
+-- 	sources = cmp.config.sources({
+-- 		--   { name = "path", option = {
+-- 		--     ignore_cmds = { "Man", "!" },
+-- 		--   } },
+--
+-- 		{
+-- 			name = "cmdline",
+-- 			option = {
+-- 				ignore_cmds = { "Man", "!" },
+-- 			},
+-- 		},
+-- 	}),
+-- })
+
 cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "path" },
-    }, {
-        {
-            name = "cmdline",
-            option = {
-                ignore_cmds = { "Man", "!" },
-            },
-        },
-    }),
+  sources = cmp.config.sources({
+
+    {
+      name = "cmdline",
+      option = {
+        ignore_cmds = { "Man", "!", "edit", "read" },
+      },
+    },
+    {
+      name = "fuzzy_path",
+      option = {
+        fd_cmd = { "fd", "-d", "20", "-p" },
+      },
+    },
+  }),
 })
 
 -- / search
 cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = "buffer" },
-    },
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
 })
 
 require("cmp_git").setup({
-    filetypes = { "gitcommit", "octo" },
-    remotes = { "origin", "upstream" },
+  filetypes = { "gitcommit", "octo" },
+  remotes = { "origin", "upstream" },
 })
