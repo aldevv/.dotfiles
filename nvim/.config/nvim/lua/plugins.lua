8b64595992f5746a1bdbcba200b2cf068ff6abd5
Alejandro Bernal <jbernalxyz@gmail.com>
2023-12-20 15:45:01 -0500

    fix fzf dependency

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -37,9 +37,28 @@ return {
     version = "*",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = req("core.telescope"),
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      if vim.fn.executable("fzf") then
+        require("telescope").load_extension("fzf")
+      end
+      -- https://github.com/nvim-telescope/telescope-fzf-native.nvim
+      -- see fzf goodies like:
+      -- 'wild 	exact-match (quoted) 	Items that include wild
+      -- ^music 	prefix-exact-match 	Items that start with music
+      -- .mp3$ 	suffix-exact-match 	Items that end with .mp3
+      -- !fire 	inverse-exact-match 	Items that do not include fire
+      -- !^music 	inverse-prefix-exact-match 	Items that do not start with music
+      -- !.mp3$ 	inverse-suffix-exact-match 	Items that do not end with .mp3
+    end,
+    build = "make"
   },
 
   {
