3fb5ebb4625a285ef1e442ba409039df852e4139
Alejandro Bernal <jbernalxyz@gmail.com>
2023-12-20 16:02:00 -0500

    fix fzf 9

--- nvim/.config/nvim/lua/core/telescope.lua
+++ nvim/.config/nvim/lua/core/telescope.lua
@@ -157,7 +157,7 @@ local setup = {
 }
 require("telescope").setup(setup)
 
-if vim.fn.executable("fzf") then
+if vim.fn.executable("fzf") and vim.loop.os_uname().sysname == "Linux" then
   require("telescope").load_extension("fzf")
 end
 
