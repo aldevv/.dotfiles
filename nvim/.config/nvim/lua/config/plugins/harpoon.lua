e7bbed239699ae72b801f41f56d965b77f1c4a04
Alejandro Bernal <jbernalxyz@gmail.com>
2024-01-19 21:27:29 -0500

    updated harpoon so its easier to read key logic

--- nvim/.config/nvim/lua/config/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/config/plugins/harpoon.lua
@@ -1,33 +1,34 @@
 local harpoon = require("harpoon")
 local extensions = require("harpoon.extensions")
 
-local one_list_per_project = true
 local cfg = {
-	settings = {
-		save_on_toggle = true,
-		sync_on_ui_close = false,
-		key = function()
-			local obj = vim.system({ "git", "config", "--get", "remote.origin.url" }, { text = true }):wait()
+  settings = {
+    save_on_toggle = true,
+    sync_on_ui_close = false,
+    key = function()
+      local obj = vim.system({ "git", "config", "--get", "remote.origin.url" }, { text = true }):wait()
 
-			if obj.code == 1 then
-				return vim.loop.cwd()
-			end
+      -- if not branch, do cwd
+      if obj.code == 1 then
+        return vim.loop.cwd()
+      end
 
-			if one_list_per_project then
-				return obj.stdout
-			end
+      local one_list_per_project = true
+      if one_list_per_project then
+        return obj.stdout
+      end
 
-			-- one list per branch
-			return vim.fn.system("git rev-parse --abbrev-ref HEAD")
-		end,
-	},
+      -- one list per branch
+      return vim.fn.system("git rev-parse --abbrev-ref HEAD")
+    end,
+  },
 }
 cfg = vim.tbl_extend("keep", cfg, require("config.plugins.harpoon_lists"))
 local ok, work = pcall(require, "work")
 if ok then
-	local projects = {}
-	projects = work.harpoon_projects
-	cfg = vim.tbl_extend("keep", cfg, projects)
+  local projects = {}
+  projects = work.harpoon_projects
+  cfg = vim.tbl_extend("keep", cfg, projects)
 end
 
 harpoon:setup(cfg)
@@ -37,21 +38,21 @@ harpoon:setup(cfg)
 
 -- extension to add keybindings for the harpoon menu
 harpoon:extend({
-	UI_CREATE = function(cx)
-		vim.keymap.set("n", "<C-v>", function()
-			harpoon.ui:select_menu_item({ vsplit = true })
-		end, { buffer = cx.bufnr })
+  UI_CREATE = function(cx)
+    vim.keymap.set("n", "<C-v>", function()
+      harpoon.ui:select_menu_item({ vsplit = true })
+    end, { buffer = cx.bufnr })
 
-		vim.keymap.set("n", "<C-x>", function()
-			harpoon.ui:select_menu_item({ split = true })
-		end, { buffer = cx.bufnr })
+    vim.keymap.set("n", "<C-x>", function()
+      harpoon.ui:select_menu_item({ split = true })
+    end, { buffer = cx.bufnr })
 
-		vim.keymap.set("n", "<C-t>", function()
-			harpoon.ui:select_menu_item({ tabedit = true })
-		end, { buffer = cx.bufnr })
-	end,
-	-- same as command_on_nav
-	-- NAVIGATE = function()
-	--     vim.cmd(cmd)
-	-- end,
+    vim.keymap.set("n", "<C-t>", function()
+      harpoon.ui:select_menu_item({ tabedit = true })
+    end, { buffer = cx.bufnr })
+  end,
+  -- same as command_on_nav
+  -- NAVIGATE = function()
+  --     vim.cmd(cmd)
+  -- end,
 })
