93b218f37386dfe5701bcf588f6cede901e86d55
Alejandro Bernal <jbernalxyz@gmail.com>
2024-10-18 11:35:40 -0500

    added working root user config

--- nvim/.config/nvim/lua/keybindings/init.lua
+++ nvim/.config/nvim/lua/keybindings/init.lua
@@ -143,37 +143,7 @@ map("n", "<leader>,sn", function()
 end, descv("edit snippet"))
 
 -- terminal
-map("n", "sñ", ":botright terminal<cr>", nor)
-map({ "n", "t" }, "<a-q>", "<cmd>ToggleTerm direction=float<cr>", nor)
-
-local Terminal = require("toggleterm.terminal").Terminal
-local ranger = Terminal:new({
-	cmd = "ranger",
-	hidden = true,
-	direction = "float",
-	float_opts = {
-		width = function()
-			return math.floor(vim.o.columns * 0.7)
-		end,
-		height = function()
-			return math.floor(vim.o.lines * 0.7)
-		end,
-	},
-})
-function _ranger_toggle()
-	ranger:toggle()
-end
-
-map({ "n", "t" }, "<a-Q>", "<cmd>lua _ranger_toggle()<cr>", nor)
-
-map({ "n", "t" }, "<a-1>", "<cmd>ToggleTerm direction=horizontal<cr>", nor)
-map({ "n", "t" }, "<a-2>", "<cmd>ToggleTerm direction=vertical size=40<cr>", nor)
-map({ "n", "t" }, "<a-3>", "<cmd>ToggleTerm direction=tab<cr>", nor)
-
--- terminal
-map("t", "<a-'>", "<c-\\><c-n>", nor_s)
-map("t", "<c-g>", "<c-\\><c-n>gT", nor_s)
-map("t", "<c-s-g>", "<c-\\><c-n>gt", nor_s)
+require("keybindings.term")
 
 -- folders
 map("n", "<F1>", ":e " .. h .. "/lua/config/keybindings/init.lua<cr>", nor_s)
