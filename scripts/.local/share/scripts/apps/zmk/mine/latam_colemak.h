b9dc66ea30e6af934dfafb2c224ff58c0b8e413a
Alejandro Bernal <jbernalxyz@gmail.com>
2024-12-04 10:12:16 -0500

    updated latam layout for adv360

--- scripts/.local/share/scripts/apps/zmk/mine/latam_colemak.h
+++ scripts/.local/share/scripts/apps/zmk/mine/latam_colemak.h
@@ -1,2 +1,44 @@
+#define LC_Q Q
+#define LC_W W
+#define LC_F E
+#define LC_P R
+#define LC_G T
+#define LC_J Y
+#define LC_L U
+#define LC_U I
+#define LC_Y O
+#define LC_NTIL P  // Ñ
+#define LC_ACU LBKT
+
+#define LC_A A
+#define LC_R S
+#define LC_S D
+#define LC_T F
+#define LC_D G
+#define LC_H H
+#define LC_N J
+#define LC_E K
+#define LC_I L
+#define LC_O SEMI
+#define LC_SQT MINUS // single quote
+
 #define LC_LBRC SQT
 #define LC_RBRC BSLH
+#define LC_Z Z
+#define LC_X X
+#define LC_C C
+#define LC_V V
+#define LC_B B
+#define LC_K N
+#define LC_M M
+#define LC_MINUS FSLH
+
+#define LC_PIPE GRAVE
+#define LC_PLUS RBKT
+#define LC_IQUE EQUAL // ¿ inverted question
+#define LC_GRAVE RA(BSLH)
+#define LC_LT NUBS // less than
+#define LC_AT RA(N2)
+#define LC_GT LS(NUBS) // greater than
+#define LC_BSLH RA(MINUS)
+#define LC_CARET RA(SQT) // ^ or tilde
