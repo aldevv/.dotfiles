265b5a970753d39329f110342048347311a4f6d7
Alejandro Bernal <jbernalxyz@gmail.com>
2025-03-10 20:33:27 -0500

    updated cmp to use blink

--- nvim/.config/nvim/lua/config/plugins/lsp/blink.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp/blink.lua
@@ -1,6 +1,18 @@
 local cmp = require('blink.cmp')
 
+
 cmp.setup({
+  sources = {
+    default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+    providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        -- make lazydev completions top priority (see `:h blink.cmp`)
+        score_offset = 100,
+      },
+    },
+  },
   keymap = {
     ["<D-c>"] = { "show" },
     ["<S-CR>"] = { "hide" },
@@ -11,6 +23,25 @@ cmp.setup({
     ["<Up>"] = { "select_prev", "fallback" },
     ["<C-n>"] = { "select_next", "fallback" },
     ["<C-p>"] = { "select_prev", "fallback" },
+
+    ["<C-d>"] = { function()
+      if not cmp.is_menu_visible() then return end
+      vim.schedule(function(opts)
+        for i = 1, 3, 1 do
+          require('blink.cmp.completion.list').select_next(opts)
+        end
+      end)
+      return true
+    end, "fallback" },
+    ["<C-u>"] = { function()
+      if not cmp.is_menu_visible() then return end
+      vim.schedule(function(opts)
+        for i = 1, 3, 1 do
+          require('blink.cmp.completion.list').select_prev(opts)
+        end
+      end)
+      return true
+    end, "fallback" },
     ["<PageDown>"] = { "scroll_documentation_down" },
     ["<PageUp>"] = { "scroll_documentation_up" },
 
@@ -18,9 +49,6 @@ cmp.setup({
   appearance = {
     use_nvim_cmp_as_default = true,
     nerd_font_variant = 'mono'
-  },
-  sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer' },
   },
   fuzzy = { implementation = "prefer_rust_with_warning" },
   completion = {
--- nvim/.config/nvim/lua/plugins/lsp-plugins.lua
+++ nvim/.config/nvim/lua/plugins/lsp-plugins.lua
@@ -11,51 +11,51 @@ return {
       { "williamboman/mason.nvim", build = ":MasonUpdate" },
       "williamboman/mason-lspconfig.nvim",
       {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-      -- {
-      --   'saghen/blink.cmp',
-      --   dependencies = { 'rafamadriz/friendly-snippets',
-      --     "saadparwaiz1/cmp_luasnip",
-      --     "onsails/lspkind-nvim",
-      --   },
-      --   version = '*',
-      --   config = req("config.plugins.lsp.blink"),
-      -- },
-
-      {
-        "hrsh7th/nvim-cmp",
-        config = req("config.plugins.lsp.cmp"),
-        dependencies = {
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets',
           "saadparwaiz1/cmp_luasnip",
-          "nvim-lua/plenary.nvim",
           "onsails/lspkind-nvim",
-          "hrsh7th/cmp-nvim-lsp",
-          -- { "hrsh7th/cmp-nvim-lua",   ft = "lua" },
-          "hrsh7th/cmp-path",
-          "hrsh7th/cmp-buffer",
-          "hrsh7th/cmp-cmdline",
-          "petertriho/cmp-git",
-          {
-            "tzachar/cmp-fuzzy-path",
-            dependencies = "tzachar/fuzzy.nvim",
-            enabled = function()
-              return vim.loop.os_uname().sysname == "Linux"
-            end,
-          },
+          "folke/lazydev.nvim",
         },
+        version = '*',
+        config = req("config.plugins.lsp.blink"),
+      },
+
+      -- {
+      --   "hrsh7th/nvim-cmp",
+      --   config = req("config.plugins.lsp.cmp"),
+      --   dependencies = {
+      --     "saadparwaiz1/cmp_luasnip",
+      --     "nvim-lua/plenary.nvim",
+      --     "onsails/lspkind-nvim",
+      --     "hrsh7th/cmp-nvim-lsp",
+      --     -- { "hrsh7th/cmp-nvim-lua",   ft = "lua" },
+      --     "hrsh7th/cmp-path",
+      --     "hrsh7th/cmp-buffer",
+      --     "hrsh7th/cmp-cmdline",
+      --     "petertriho/cmp-git",
+      --     {
+      --       "tzachar/cmp-fuzzy-path",
+      --       dependencies = "tzachar/fuzzy.nvim",
+      --       enabled = function()
+      --         return vim.loop.os_uname().sysname == "Linux"
+      --       end,
+      --     },
+      --   },
+      -- },
+    },
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
-
   {
     "nvimtools/none-ls.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "williamboman/mason.nvim" },
