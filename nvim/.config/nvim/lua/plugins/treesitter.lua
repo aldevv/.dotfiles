eb38ab9b172ebb9661ddd2d7d8ec2f1a4a7da00e
Alejandro Bernal <jbernalxyz@gmail.com>
2025-07-16 17:37:24 -0500

    add context to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,31 @@
 local function req(module)
-	return function(name, opts)
-		require(module)
-	end
+  return function(name, opts)
+    require(module)
+  end
 end
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = req("config.plugins.treesitter"),
-	},
-	{
-		-- failing for angular, shows function signature, on top
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			vim.cmd("hi TreesitterContextLineNumber gui=bold guifg=orange")
-		end,
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-	},
-	"kana/vim-textobj-user",
-	{
-		"kana/vim-textobj-entire",
-		dependencies = "kana/vim-textobj-user",
-		init = function()
-			vim.g.textobj_entire_no_default_key_mappings = 1
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = req("config.plugins.treesitter"),
+  },
+  {
+    -- failing for angular, shows function signature, on top
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      vim.cmd("hi TreesitterContextLineNumber gui=bold guifg=orange")
+      require 'treesitter-context'.setup {
+        enable = true,
+      }
+    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
+  "kana/vim-textobj-user",
+  {
+    "kana/vim-textobj-entire",
+    dependencies = "kana/vim-textobj-user",
+    init = function()
+      vim.g.textobj_entire_no_default_key_mappings = 1
+    end,
+  },
 }
